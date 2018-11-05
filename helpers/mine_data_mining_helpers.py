f19405d5ac16dfc5b355db66b2cdd1c5aafcf0e5
w102060018w <w102060018w@gmail.com>
2018-11-05 11:47:58 +0800

    update

--- helpers/mine_data_mining_helpers.py
+++ helpers/mine_data_mining_helpers.py
@@ -1,22 +1,20 @@
 import nltk
 
 """
-Helper functions for data mining lab session 2018 Fall Semester
-Author: Elvis Saravia
-Email: ellfae@gmail.com
+Helper functions for data mining lab session 2017 Fall
+
+Notations:
+d - document
+D - documents
+V - vowels
+w - word
+W - words
+l - letter
 """
 
-def format_rows(docs):
-    """ format the text field and strip special characters """
-    D = []
-    for d in docs.data:
-        temp_d = " ".join(d.split("\n")).strip('\n\t')
-        D.append([temp_d])
-    return D
-
-def format_labels(target, docs):
+def format_labels(label_val, dic):
     """ format the labels """
-    return docs.target_names[target]
+    return dic[label_val]
 
 def check_missing_values(row):
     """ functions that check and verifies if there are missing values in dataframe """
